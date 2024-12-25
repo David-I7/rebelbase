01b786c25cb6cac3ed38dce4a0f3a574aef33209
david <150824323+David-I7@users.noreply.github.com>
2024-12-25 17:09:20 +0200

    Finish adding skeletons to slow components (#20)
    * Make home skeleton page
    
    * Remove redis dump.rdb from source control
    
    ---------
    
    Co-authored-by: David <daviodiosub09@gmail.com>

---
+++ src/_components/skeletons/HomeSkeleton.tsx
@@ -0,0 +1,103 @@
+import VerticalListSkeleton from "@/_components/skeletons/list/VerticalListItemSkeleton";
+import VerticalListSection from "@/app/_components/gameRepresentation/list/VerticalListSection";
+import VerticalRay from "@/_components/VerticalRay";
+import FixedSizeCarousel from "@/_components/nonPrimitives/carousel/FixedSizeCarousel";
+import { MdChevronRight } from "react-icons/md";
+import VerticalCardSkeleton from "@/_components/skeletons/cards/VerticalCardSkeleton";
+import CardDetailsSkeleton from "@/_components/skeletons/cards/CardDetailsSkeleton";
+import HorizontalCardSkeleton from "@/_components/skeletons/cards/HorizontalCard";
+import HeroSectionSkeleton from "@/_components/skeletons/heroSection/HeroSectionSkeleton";
+
+function createListFromComponent(
+  Component: () => React.JSX.Element,
+  itemCount: number,
+  key: string
+): React.JSX.Element[] {
+  const components: ReturnType<typeof createListFromComponent> = [];
+
+  for (let i = 0; i < itemCount; i++) {
+    components.push(<Component key={`${key}_${i}`} />);
+  }
+
+  return components;
+}
+
+export default function LoadingHome() {
+  return (
+    <main>
+      <div className="max-w-[1280px] mx-auto mt-8">
+        <HeroSectionSkeleton />
+      </div>
+      <div className="flex flex-col [@media(min-width:1344px)]:ml-[calc((100vw_-_1280px)_/_2)] ml-4 md:ml-8">
+        <FixedSizeCarousel>
+          <div className="flex items-center mb-4 mt-20">
+            <h2 className="flex items-center text-on-surface-heading h-10 text-2xl">
+              Most Anticipated
+            </h2>
+            <MdChevronRight size={32} className="text-on-surface-heading" />
+          </div>
+          <ul className="inline-grid grid-rows-1 grid-cols-[repeat(15,200px)] md:grid-cols-[repeat(15,254px)] lg:grid-cols-[repeat(15,308px)] gap-4 pr-4 lg:pr-8 [@media(min-width:1280px)]:pr-[calc((100vw_-_1280px)_/_2)]">
+            {createListFromComponent(
+              () => (
+                <HorizontalCardSkeleton>
+                  <CardDetailsSkeleton type="FIRST_RELEASE_DATE" />
+                </HorizontalCardSkeleton>
+              ),
+              15,
+              "most_anticipated_skeleton"
+            )}
+          </ul>
+        </FixedSizeCarousel>
+        <FixedSizeCarousel>
+          <div className="flex items-center mb-4 mt-20">
+            <h2 className="flex items-center text-on-surface-heading h-10 text-2xl">
+              Top Rated
+            </h2>
+            <MdChevronRight size={32} className="text-on-surface-heading" />
+          </div>
+          <ul className="inline-grid grid-rows-1 grid-cols-[repeat(15,minmax(164px,1fr))] md:grid-cols-[repeat(15,minmax(204px,1fr))] lg:grid-cols-[repeat(15,minmax(244px,1fr))] gap-4 overflow-x-auto carousel pr-4 lg:pr-8 [@media(min-width:1280px)]:pr-[calc((100vw_-_1280px)_/_2)]">
+            {createListFromComponent(
+              () => (
+                <VerticalCardSkeleton>
+                  <CardDetailsSkeleton type="RATING" />
+                </VerticalCardSkeleton>
+              ),
+              15,
+              "top_rated_games_skeleton"
+            )}
+          </ul>
+        </FixedSizeCarousel>
+      </div>
+      <div className="max-w-[1280px] [@media(min-width:1344px)]:mx-auto ml-4 md:ml-8">
+        <VerticalListSection>
+          <VerticalListSkeleton sectionName="Offline Games" />
+          <VerticalRay />
+          <VerticalListSkeleton sectionName="Online Games" />
+          <VerticalRay />
+          <VerticalListSkeleton sectionName="Casual Games" />
+        </VerticalListSection>
+      </div>
+      <div className="mt-20 [@media(min-width:1344px)]:ml-[calc((100vw_-_1280px)_/_2)] ml-4 md:ml-8">
+        <FixedSizeCarousel>
+          <div className="flex items-center mb-4">
+            <h2 className="flex items-center text-on-surface-heading h-10 text-2xl">
+              Upcoming Releases
+            </h2>
+            <MdChevronRight size={32} className="text-on-surface-heading" />
+          </div>
+          <ul className="inline-grid grid-rows-1 grid-cols-[repeat(15,200px)] md:grid-cols-[repeat(15,254px)] lg:grid-cols-[repeat(15,308px)] gap-4 pr-4 lg:pr-8 [@media(min-width:1280px)]:pr-[calc((100vw_-_1280px)_/_2)]">
+            {createListFromComponent(
+              () => (
+                <HorizontalCardSkeleton>
+                  <CardDetailsSkeleton type="FIRST_RELEASE_DATE" />
+                </HorizontalCardSkeleton>
+              ),
+              15,
+              "upcoming_releases_skeleton"
+            )}
+          </ul>
+        </FixedSizeCarousel>
+      </div>
+    </main>
+  );
+}
---
+++ src/_components/skeletons/cards/HorizontalCard.tsx
@@ -0,0 +1,14 @@
+import React, { ReactNode } from "react";
+
+const HorizontalCardSkeleton = ({ children }: { children: ReactNode }) => {
+  return (
+    <li className="h-full">
+      <div className="max-w-[308px] h-full flex flex-col">
+        <div className=" max-w-[308px] aspect-video bg-surface-container-normal rounded-xl overflow-hidden mb-3"></div>
+        {children}
+      </div>
+    </li>
+  );
+};
+
+export default HorizontalCardSkeleton;
