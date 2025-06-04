a967896d902eb7c3c13d16c734c5c84a32900b17
David <daviodiosub09@gmail.com>
2025-06-04 16:04:44 +0300

    add sharp as an optional dependency

--- src/data/config/requestOptions.ts
+++ src/data/config/requestOptions.ts
@@ -6,14 +6,3 @@ export const IGDBRequestOptions: RequestInit = {
     "Client-ID": process.env.TWITCH_CLIENT_ID!,
   },
 };
-
-export const worldNewsApiRequestOptions: RequestInit = {
-  headers: {
-    "x-api-key": process.env.WORLD_NEWS_API_KEY!,
-  },
-};
-export const apiLeagueRequestOptions: RequestInit = {
-  headers: {
-    "x-api-key": process.env.API_LEAGUE_API_KEY!,
-  },
-};
